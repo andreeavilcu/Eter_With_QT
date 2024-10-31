27b34b54f50cd2cce5ad23f299a8ccc6a2852818
andreiprb <andreipriboi@icloud.com>
2024-10-31 15:37:50 +0200

    more main tests with uniform distribution

--- main.cpp
+++ main.cpp
@@ -1,7 +1,13 @@
+#include <random>
+
 #include "models/Player.h"
 
-int main()
-{
-    Player player({}, rand() % Wizard::wizard_count);
+int main() {
+    std::random_device rd;
+    std::mt19937 gen(rd());
+
+    std::uniform_int_distribution<int> wizardDistribution(0, Wizard::wizard_count);
+
+    const Player player({}, wizardDistribution(gen));
     player.playWizard();
 }
