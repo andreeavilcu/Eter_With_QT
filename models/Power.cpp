c888cfe496158e0cf16e138462352afc2ca5390e
RuxandraUrs <ruxandraurs@gmail.com>
2024-11-02 13:01:52 +0200

    Implemented methods for class Power in "Power.cpp"

---
+++ models/Power.cpp
@@ -0,0 +1,122 @@
+#include "Power.h"
+
+void Power::PowerAction::controlledExplosion()
+{
+	std::cout << "controlledExplosion" << std::endl;
+}
+
+void Power::PowerAction::destruction()
+{
+	std::cout << "destruction" << std::endl;
+}
+
+void Power::PowerAction::flame()
+{
+	std::cout << "flame" << std::endl;
+}
+
+void Power::PowerAction::fire()
+{
+	std::cout << "fire" << std::endl;
+}
+
+void Power::PowerAction::ash()
+{
+	std::cout << "ash" << std::endl;
+}
+
+void Power::PowerAction::spark()
+{
+	std::cout << "spark" << std::endl;
+}
+
+void Power::PowerAction::squall()
+{
+	std::cout << "squall" << std::endl;
+}
+
+void Power::PowerAction::gale()
+{
+	std::cout << "gale" << std::endl;
+}
+
+void Power::PowerAction::hurricane()
+{
+	std::cout << "hurricane" << std::endl;
+}
+
+void Power::PowerAction::gust()
+{
+	std::cout << "gust" << std::endl;
+}
+
+void Power::PowerAction::mirage()
+{
+	std::cout << "mirage" << std::endl;
+
+}
+
+void Power::PowerAction::storm()
+{
+	std::cout << "storm" << std::endl;
+}
+
+void Power::PowerAction::tide()
+{
+	std::cout << "tide" << std::endl;
+}
+
+void Power::PowerAction::mist()
+{
+	std::cout << "mist" << std::endl;
+}
+
+void Power::PowerAction::wave()
+{
+	std::cout << "wave" << std::endl;
+}
+
+void Power::PowerAction::whirlpool()
+{
+std::cout << "whirlpool" << std::endl;
+}
+
+void Power::PowerAction::blizzard()
+{
+	std::cout << "blizzard" << std::endl;
+}
+
+void Power::PowerAction::waterfall()
+{
+	std::cout << "waterfall" << std::endl;
+}
+
+void Power::PowerAction::support()
+{
+	std::cout << "support" << std::endl;
+}
+
+void Power::PowerAction::earthquake()
+{
+	std::cout << "earthquake" << std::endl;
+}
+
+void Power::PowerAction::crumble()
+{
+	std::cout << "crumble" << std::endl;
+}
+
+void Power::PowerAction::border()
+{
+	std::cout << "border" << std::endl;
+}
+
+void Power::PowerAction::avalanche()
+{
+	std::cout << "avalanche" << std::endl;
+}
+
+void Power::PowerAction::rock()
+{
+	std::cout << "rock" << std::endl;
+}
