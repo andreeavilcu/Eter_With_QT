be1ce42c81e2a6f3eed89079453cc55821b3b45d
RuxandraUrs <ruxandraurs@gmail.com>
2024-11-06 11:23:36 +0200

    idea for memorizing the positions of a card placed by a player

--- models/Card.cpp
+++ models/Card.cpp
@@ -1,14 +1,18 @@
 #include "Card.h"
 
-Card::Card(const Value _value, const Color _color)
+Card::Card(const Value _value, const Color _color, const ID _id)
 	: m_value{ _value },
-	m_color{ _color } {
+	m_color{ _color },
+	m_playerID{ _id } {
 }
 
-Card::Card(Card&& other) noexcept : m_value(other.m_value) {
+Card::Card(Card&& other) noexcept
+	: m_value(other.m_value), m_color(other.m_color), m_playerID(other.m_playerID) {
 	other.m_value = Value::Eter;
 	other.m_color = Color::Undefined;
+	other.m_playerID = ID::StartID;
 }
+
 
 Card& Card::operator=(Card&& other) noexcept {
 	if (this != &other) {
@@ -26,6 +30,14 @@ Card::Value Card::getValue() const {
 
 Card::Color Card::getColor() const {
 	return m_color;
+}
+
+Card::ID Card::getPlayerID() const{
+	return m_playerID;
+}
+
+void Card::setPlayerID(ID _id) {
+	m_playerID  = _id;
 }
 
 void Card::setColor(const Color _color) {
--- models/Card.h
+++ models/Card.h
@@ -20,8 +20,13 @@ public:
         Red,
         Blue,
     };
+    enum class ID : size_t {
+        StartID = 0,
+        Player1,
+        Player2,
+    };
 
-    explicit Card(Value _value, Color _color = Color::Undefined);
+    explicit Card(Value _value, Color _color = Color::Undefined, ID _id=ID::StartID);
     ~Card() = default;
     Card(const Card& other) = default;
     Card& operator=(const Card& other) = default;
@@ -30,14 +35,17 @@ public:
 
     [[nodiscard]] Value getValue() const;
     [[nodiscard]] Color getColor() const;
+    [[nodiscard]] ID getPlayerID() const;
 
+    void setPlayerID(ID _id);
     friend std::ostream& operator<<(std::ostream& os, const Card& card);
     friend class Player;
+
 
 private:
     Value m_value;
     Color m_color;
-
+    ID m_playerID;
     void setColor(Color _color);
 
 };
