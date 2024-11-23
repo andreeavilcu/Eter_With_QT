9591343838b2667f13c574785f53d1d90d23694e
andreiprb <andreipriboi@icloud.com>
2024-11-23 17:17:57 +0200

    fixed controlledExplosion function and cleaned code

--- models/Power.cpp
+++ models/Power.cpp
@@ -3,59 +3,29 @@
 #include "Game.h"
 
 bool Power::PowerAction::controlledExplosion(Player& _player, Game& _game) {
-    auto& board = _game.m_board.m_board;
+    Game::Board& board = _game.m_board;
 
-    if (_game.m_playedExplosion) {
-        auto explosionEffects = _game.generateExplosion(_game.m_board.getSize());
-        auto returnedCards = _game.m_board.useExplosion(explosionEffects);
+    auto explosionEffects = _game.generateExplosion(board.getSize());
+    auto returnedCards = board.useExplosion(explosionEffects);
 
-        for (auto& card : returnedCards) {
-            if (card.getColor() == _player.getColor()) {
-                _player.returnCard(card);
-            } else {
-                auto& opponent = (_player.getColor() == Card::Color::Player1) ? _game.m_player2 : _game.m_player1;
-                opponent.returnCard(card);
-            }
-        }
+    auto& opponent = _player.getColor() == Card::Color::Player1 ? _game.m_player2 : _game.m_player1;
 
-        return true;
-    }
-
-    auto explosionEffects = _game.generateExplosion(_game.m_board.getSize());
-
-    for (size_t row = 0; row < board.size(); ++row) {
-        for (size_t col = 0; col < board.size(); ++col) {
-            if (explosionEffects[row][col] != Game::ExplosionEffect::None) {
-                auto tempStack = std::move(board[row][col]);
-                board[row][col].clear();
-
-                if (!_game.m_board.checkBoardIntegrity()) {
-                    board[row][col] = std::move(tempStack);
-                    explosionEffects[row][col] = Game::ExplosionEffect::None;
-                }
-            }
-        }
-    }
-
-    auto returnedCards = _game.m_board.useExplosion(explosionEffects);
-
-    for (auto& card : returnedCards) {
-        if (card.getColor() == _player.getColor()) {
+    for (auto& card : returnedCards)
+        if (card.getColor() == _player.getColor())
             _player.returnCard(card);
-        } else {
-            auto& opponent = (_player.getColor() == Card::Color::Player1) ? _game.m_player2 : _game.m_player1;
+
+        else
             opponent.returnCard(card);
-        }
-    }
 
     _game.m_playedExplosion = true;
 
     return true;
 }
 
-
 bool Power::PowerAction::destruction(Player& _player, Game& _game) {
-    auto& opponent = (_player.getColor() == Card::Color::Player1) ? _game.m_player2 : _game.m_player1;
+    Game::Board& board = _game.m_board;
+
+    auto& opponent = _player.getColor() == Card::Color::Player1 ? _game.m_player2 : _game.m_player1;
 
     auto [lastRow, lastCol] = opponent.getLastPlacedCard();
 
@@ -63,15 +33,15 @@ bool Power::PowerAction::destruction(Player& _player, Game& _game) {
         return false;
     }
 
-    if (!_game.m_board.checkIndexes(lastRow, lastCol) || _game.m_board.m_board[lastRow][lastCol].empty()) {
+    if (!board.checkIndexes(lastRow, lastCol) || board.m_board[lastRow][lastCol].empty()) {
         return false;
     }
 
-    auto& stack = _game.m_board.m_board[lastRow][lastCol];
+    auto& stack = board.m_board[lastRow][lastCol];
     Card affectedCard = std::move(stack.back());
     stack.pop_back();
 
-    if (!_game.m_board.checkBoardIntegrity()) {
+    if (!board.checkBoardIntegrity()) {
         stack.push_back(std::move(affectedCard));
         return false;
     }
@@ -80,12 +50,14 @@ bool Power::PowerAction::destruction(Player& _player, Game& _game) {
 }
 
 bool Power::PowerAction::flame(Player& _player, Game& _game) {
+    Game::Board& board = _game.m_board;
+
     size_t illusionRow = -1, illusionCol = -1;
     bool illusionFound = false;
 
-    for (size_t row = 0; row < _game.m_board.getSize(); ++row) {
-        for (size_t col = 0; col < _game.m_board.getSize(); ++col) {
-            if (_game.m_board.checkIllusion(row, col, _player.getColor() == Card::Color::Player1 ? Card::Color::Player2 : Card::Color::Player1)) {
+    for (size_t row = 0; row < board.getSize(); ++row) {
+        for (size_t col = 0; col < board.getSize(); ++col) {
+            if (board.checkIllusion(row, col, _player.getColor() == Card::Color::Player1 ? Card::Color::Player2 : Card::Color::Player1)) {
                 illusionRow = row;
                 illusionCol = col;
                 illusionFound = true;
@@ -96,7 +68,7 @@ bool Power::PowerAction::flame(Player& _player, Game& _game) {
     }
 
     if (illusionFound) {
-        _game.m_board.resetIllusion(illusionRow, illusionCol);
+        board.resetIllusion(illusionRow, illusionCol);
     }
 
     size_t x, y, int_value;
@@ -111,7 +83,7 @@ bool Power::PowerAction::flame(Player& _player, Game& _game) {
         return false;
     }
 
-    _game.m_board.placeCard(x, y, std::move(*playedCard));
+    board.placeCard(x, y, std::move(*playedCard));
     _player.placeCard(x, y);
     return true;
 }
@@ -372,7 +344,8 @@ bool Power::PowerAction::tsunami(Player& _player, Game& _game) {
     else {
         board.m_restrictedCol = index;
     }
-    board.justBlocked = true;
+
+    board.m_justBlocked = true;
     return true;
 }
 
